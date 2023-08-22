ae4a8bf11828f6ec87dd66d75bd238131a070cd3
Fikret Gezer <64322071+FikretGezer@users.noreply.github.com>
2023-08-22 21:11:06 +0300

    Refactoring the codes

--- Assets/_NoNameSurvivorAssets/_Scripts/CameraMovement.cs
+++ Assets/_NoNameSurvivorAssets/_Scripts/CameraMovement.cs
@@ -6,36 +6,23 @@ namespace FikretGezer
 {
     public class CameraMovement : MonoBehaviour
     {
+        [SerializeField] private Vector3 _distanceBetweenPlayerAndCamera;
         [SerializeField] private Transform _playerBody;
         [SerializeField] private float _mouseSpeed = 100f;
         [SerializeField] private float _lerpSpeed = 2f;
-        [SerializeField] private Vector3 _mesafe;
-
-
-        [SerializeField] private LayerMask _layer;
-        [SerializeField] private GameObject _object;
-        private float xRotation;
         private void Awake() {
             //Cursor.lockState = CursorLockMode.Locked;
         }
         private void Update() {
-            float mouseX = Input.GetAxis("Mouse X") * _mouseSpeed * Time.deltaTime;
-
+            FollowPlayer();            
+        }
+        private void FollowPlayer()
+        {
             var posPlayer = _playerBody.position;
             posPlayer.y = transform.position.y;
-            posPlayer += _mesafe;
+            posPlayer += _distanceBetweenPlayerAndCamera;
 
             transform.position = Vector3.Lerp(transform.position, posPlayer, _lerpSpeed * Time.deltaTime); 
-            //_playerBody.Rotate(Vector3.up * mouseX);
-            
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, _layer))
-            {
-                _object.transform.position = hit.point;
-            }
-            var lookPos = _object.transform.position;
-            lookPos.y = _playerBody.transform.position.y;
-            _playerBody.LookAt(lookPos);
         }
     }
 }
--- Assets/_NoNameSurvivorAssets/_Scripts/PlayerController.cs
+++ Assets/_NoNameSurvivorAssets/_Scripts/PlayerController.cs
@@ -11,6 +11,7 @@ namespace FikretGezer
         [SerializeField] private float _jumpHeight = 2f;
         [SerializeField] private float _jumpSpeed = -2f;
         [SerializeField] private float _fallSpeed = 2f;
+        [SerializeField] private Transform _object;
 
         [Header("Gravity Things")]
         [SerializeField] private float _gravity = -9.81f;
@@ -19,33 +20,65 @@ namespace FikretGezer
         [SerializeField] private float _groundDistance;
 
         private CharacterController _characterController;
-        Vector3 _velocity;
+        private Vector3 _velocity;
+        private Camera _cam;
+        
+        private bool _isGrounded;
+        private bool _isJumping;
 
         private void Awake() {
             _characterController = GetComponent<CharacterController>();
+            _cam = Camera.main;
         }
         private void Update() {
             
             float hor = Input.GetAxis("Horizontal");
             float ver = Input.GetAxis("Vertical");
-            bool isGrounded = IsGrounded();
-            if(isGrounded && _velocity.y < 0)
+            _isGrounded = IsGrounded();
+
+            Controls();
+            Moving(hor, ver);
+            RotateCharacter();
+            Jumping();
+        }
+        private void Controls()
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                _isJumping = true;
+            } 
+        }
+        private void Jumping()
+        {
+            if(_isGrounded && _velocity.y < 0)
             {
                 _velocity.y = -2f;
             }
-            
-            //Vector3 move = transform.right * hor + transform.forward * ver;
-            Vector3 move = new Vector3(hor, 0, ver);
-            _characterController.Move(move * _moveSpeed * Time.deltaTime);
-
-            if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            if(_isJumping && _isGrounded)
             {
                 float eq = _jumpHeight * _jumpSpeed * _gravity;
                 _velocity.y = Mathf.Sqrt(eq);
+                _isJumping = false;
             }
 
             _velocity.y += _gravity * _fallSpeed * Time.deltaTime;
             _characterController.Move(_velocity * Time.deltaTime);
+        }
+        private void Moving(float hor, float ver)
+        {
+            Vector3 move = new Vector3(hor, 0, ver);
+            _characterController.Move(move * _moveSpeed * Time.deltaTime);
+        }
+        private void RotateCharacter()
+        {
+            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, _groundLayer))
+            {
+                _object.transform.position = hit.point;
+            }
+            var pos = _object.transform.position;
+            pos.y = transform.position.y;
+            transform.LookAt(pos);
         }
         private bool IsGrounded()
         {
